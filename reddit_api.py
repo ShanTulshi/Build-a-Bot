cb64017084be496ed92dfa721650bb87b2283263
Hanna Parker <hparker2@illinois.edu>
2017-04-13 23:13:40 -0500

    woot

--- reddit_api.py
+++ reddit_api.py
@@ -3,10 +3,14 @@ import requests
 
 def get_comments(reddit_username):
     SECRET = 'Zn7Q6uj2KfarooUuK_jEoMEv0kg'
-    headers = {"Authorization": "bearer 3rhJj1Zx1Hwu0p9djavecclBVpc", "User-Agent": "Build-A-Bot Markov Workshop by hannparkk"}
+    headers = {"Authorization": "bearer MKiE5F9xEthyS51_9Q30Dv2acC4", "User-Agent": "Build-A-Bot Markov Workshop by hannparkk"}
     endpoint = "https://oauth.reddit.com/user/" + reddit_username + "/comments/.json"
     response = requests.get(endpoint, headers=headers)
     r = json.loads(response.text)
-    return r
+    array = r[u'data'][u'children']
+    comments = []
+    for entry in array:
+        comments.append(entry[u'data'][u'body'])
+    return comments
 
 print(get_comments('Avesplosion'))
